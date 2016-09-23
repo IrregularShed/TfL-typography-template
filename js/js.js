3afa39e43ca6530b3ee14fc4131aa05e63758439
didacus <diegooriani@Diegos-MacBook-Pro.local>
2016-09-23 14:05:11 +0100

    added responsive layout + header selector + block quote

--- js/js.js
+++ js/js.js
@@ -1,7 +1,61 @@
-document.getElementById('formEntry').addEventListener('submit', function (e) {
-  e.preventDefault();
-  					var input = document.getElementById("hex");
-  						var inputtedColour = input.value;
-	  $("body").css("background", inputtedColour);
-      console.log(inputtedColour)
+
+$(function() {
+  var weights = ["R","M","H","T"];
+
+  var currentIndex = 0;
+
+  $('#h1').on('click', function(e) {
+    $("h1").removeClass().addClass(weights[currentIndex]);
+    if (currentIndex === weights.length) {
+        currentIndex = 0;
+    } else {
+      currentIndex++;
+    }
+
+    e.preventDefault();
+  });
+
+  $('#h2').on('click', function(e) {
+    $("h2").removeClass().addClass(weights[currentIndex]);
+    if (currentIndex === weights.length) {
+        currentIndex = 0;
+    } else {
+      currentIndex++;
+    }
+
+    e.preventDefault();
+  });
+
+  $('#h3').on('click', function(e) {
+    $("h3").removeClass().addClass(weights[currentIndex]);
+    if (currentIndex === weights.length) {
+        currentIndex = 0;
+    } else {
+      currentIndex++;
+    }
+
+    e.preventDefault();
+  });
+
+  $('#h4').on('click', function(e) {
+    $("h4").removeClass().addClass(weights[currentIndex]);
+    if (currentIndex === weights.length) {
+        currentIndex = 0;
+    } else {
+      currentIndex++;
+    }
+
+    e.preventDefault();
+  });
+
+  $('#p').on('click', function(e) {
+    $("p").removeClass().addClass(weights[currentIndex]);
+    if (currentIndex === weights.length) {
+        currentIndex = 0;
+    } else {
+      currentIndex++;
+    }
+
+    e.preventDefault();
+  });
 });
